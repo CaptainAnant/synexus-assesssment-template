516d6c2e0444f1f42454fb25e890cdb1824a44dd
Anant <sharmaanant249@gmail.com>
2026-04-07 09:44:13 +0530

    Add function to sum positive numbers in an array
    Implement a function to sum positive integers in an array.

---
+++ challange.js
@@ -0,0 +1,14 @@
+/**
+ * SYNEXUS CODING CHALLENGE
+ * * Write a function that takes an array of integers and returns 
+ * the sum of all positive numbers in the array.
+ * * Example: [1, -4, 7, 12] => 20
+ */
+
+function sumPositiveNumbers(arr) {
+    // Write your logic here
+
+}
+
+// Do not modify the export, it is used for grading
+module.exports = sumPositiveNumbers;
